2992fd7be669e3d47ae3ba8f9f9db0ea145e85bf
Tobias Sandström <disguised@live.se>
2022-05-23 13:47:57 +0200

    Created CategoryUpdateModel prepared with validation

---
+++ Kentaka-Webshop/Models/CategoryModels/CategoryUpdateModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kentaka_Webshop.Models.CategoryModels
+{
+    public class CategoryUpdateModel
+    {
+        public CategoryUpdateModel()
+        {
+        }
+
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "You have to enter the old {0}")]
+        public string OldCategoryName { get; set; }
+
+
+        [Display(Name ="category name")]
+        [Required(ErrorMessage = "You have to enter the new {0}")]
+        [StringLength(50, ErrorMessage = "{0} must be atleast {2} characters long", MinimumLength = 2)]
+        public string NewCategoryName { get; set; }
+    }
+}
