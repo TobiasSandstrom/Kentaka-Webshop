9d31b50b62f51686ce8d509632c66a3c97effaca
Tobias Sandström <disguised@live.se>
2022-05-23 15:45:35 +0200

    added Get View for Contact

--- Kentaka-Webshop/Controllers/ContactController.cs
+++ Kentaka-Webshop/Controllers/ContactController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Kentaka_Webshop.Managers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Kentaka_Webshop.Controllers
 {
     public class ContactController : Controller
     {
+        public readonly CategoryManager _categorymManager;
+
+        public ContactController(CategoryManager categorymManager)
+        {
+            _categorymManager = categorymManager;
+        }
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var categories = _categorymManager.GetAllAsync();
+            return View(categories);
         }
+        
     }
 }
