2410fbf73e8ac4c8b4e84bf163d661d8494aa22f
Tobias Sandström <disguised@live.se>
2022-05-23 13:36:00 +0200

    Created CategoryManager and its interface

---
+++ Kentaka-Webshop/Managers/CategoryManager.cs
@@ -0,0 +1,16 @@
+﻿using Kentaka_Webshop.Entity;
+
+namespace Kentaka_Webshop.Managers
+{
+    public class CategoryManager
+    {
+        public interface ICategoryManager
+        {
+            Task<CategoryResult> CreateAsync(CategoryCreateModel model);
+            Task<CategoryResult> UpdateAsync(CategoryUpdateModel model);
+            Task<CategoryViewModel> GetOne(int id);
+            Task<List<CategoryViewModel>> GetAllAsync();
+            Task DeleteAsync(int id);
+        }
+    }
+}
