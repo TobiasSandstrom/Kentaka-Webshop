aac15e873270967bfb11d645a81c55c26fc99907
Tobias Sandström <disguised@live.se>
2022-05-23 13:55:28 +0200

    Added _context and implemented ICategoryInterface

--- Kentaka-Webshop/Managers/CategoryManager.cs
+++ Kentaka-Webshop/Managers/CategoryManager.cs
@@ -1,10 +1,9 @@
-﻿using Kentaka_Webshop.Entity;
+﻿using Kentaka_Webshop.Data;
+using Kentaka_Webshop.Entity;
 using Kentaka_Webshop.Models.CategoryModels;
 
 namespace Kentaka_Webshop.Managers
 {
-    public class CategoryManager
-    {
         public interface ICategoryManager
         {
             Task<CategoryResult> CreateAsync(CategoryCreateModel model);
@@ -12,6 +11,42 @@ namespace Kentaka_Webshop.Managers
             Task<CategoryViewModel> GetOne(int id);
             Task<List<CategoryViewModel>> GetAllAsync();
             Task DeleteAsync(int id);
+        }
+
+    public class CategoryManager : ICategoryManager
+    {
+
+        private readonly SqlDbContext _context;
+
+        public CategoryManager(SqlDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public Task<CategoryResult> CreateAsync(CategoryCreateModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<CategoryViewModel>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CategoryViewModel> GetOne(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CategoryResult> UpdateAsync(CategoryUpdateModel model)
+        {
+            throw new NotImplementedException();
         }
     }
 }
