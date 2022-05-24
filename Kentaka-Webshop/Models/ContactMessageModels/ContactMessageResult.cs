d2fdf72458af10e5dfb3fdcacb226ca02a442e75
Tobias Sandström <disguised@live.se>
2022-05-24 11:06:40 +0200

    Created ContactMessageResult

---
+++ Kentaka-Webshop/Models/ContactMessageModels/ContactMessageResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Kentaka_Webshop.Models.ContactMessageModels
+{
+    public class ContactMessageResult
+    {
+        public bool Result { get; set; } = false;
+        public string Message { get; set; } = string.Empty;
+    }
+}
