45e93ebe381ae167eec1ffc075c2390faab14b26
Tobias Sandström <disguised@live.se>
2022-05-24 11:05:16 +0200

    Created ContactMessageViewModel

---
+++ Kentaka-Webshop/Models/ContactMessageModels/ContactMessageViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Kentaka_Webshop.Models.ContactMessageModels
+{
+    public class ContactMessageViewModel
+    {
+        public int id { get; set; }
+        public string CategoryName { get; set; }
+        public string Subject { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public string Message { get; set; }
+        public bool HasBeenAnswered { get; set; }
+    }
+}
