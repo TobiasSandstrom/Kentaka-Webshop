79cfdfebf66a87f803eafea3f5450ca89e085208
Tobias Sandström <disguised@live.se>
2022-05-24 12:46:26 +0200

    Created CategoriesAndContactResultModel

---
+++ Kentaka-Webshop/Models/CategoriesAndContactResultModel.cs
@@ -0,0 +1,11 @@
+﻿using Kentaka_Webshop.Models.CategoryModels;
+using Kentaka_Webshop.Models.ContactMessageModels;
+
+namespace Kentaka_Webshop.Models
+{
+    public class CategoriesAndContactResultModel
+    {
+        public List<CategoryViewModel>? Categories { get; set; }
+        public ContactMessageResult? Result { get; set; }
+    }
+}
