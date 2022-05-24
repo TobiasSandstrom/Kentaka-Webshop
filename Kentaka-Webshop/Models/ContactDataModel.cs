2cf4f5d7febbc897bedeb4a8262b9994d6cae5fb
Tobias Sandström <disguised@live.se>
2022-05-24 23:04:21 +0200

    Added Constructor

--- Kentaka-Webshop/Models/ContactDataModel.cs
+++ Kentaka-Webshop/Models/ContactDataModel.cs
@@ -5,6 +5,10 @@ namespace Kentaka_Webshop.Models
 {
     public class ContactDataModel
     {
+        public ContactDataModel()
+        {
+        }
+
         public ContactMessageForm Form { get; set; } = null!;
         public List<CategoryViewModel> Categories { get; set; } = null!;
         public ContactMessageResult Result { get; set; } = null!;
