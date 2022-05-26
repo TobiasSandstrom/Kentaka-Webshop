14083532b142f41fe746471d1acdcc04e9f47c1b
Tobias Sandström <disguised@live.se>
2022-05-26 11:33:56 +0200

    Createt test file and named first test

---
+++ Kentaka-Webshop-Test/CategoryManagerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kentaka_Webshop_Test
+{
+    public class CategoryManagerTest
+    {
+        [Fact]
+        public void GetAllAsync_Should_Return_List_Of_Categories()
+        {
+
+        }
+    }
+}
