2cfc27a9e856dbeb2f2f14e9501b3394d90bb123
Fleeh <turboralle@hotmail.com>
2022-05-30 18:14:32 +0200

    Added code to products.js in root, js folder

--- Kentaka-Webshop/wwwroot/js/products.js
+++ Kentaka-Webshop/wwwroot/js/products.js
@@ -1,1 +1,16 @@
-﻿
+﻿let suggestions = [
+
+    "Blue silk flare sleeved top",
+    "New look men's coat",
+    "Winter collection for women's",
+    "Red women's blazer full",
+    "Women's leather bag and shoe",
+    "Men's denim shirts full",
+    "New look men's sneakers",
+    "Circle pattern girls shirt",
+    "Gents blue lightweight trainers",
+    "Women smart high heel shoe",
+    "Gents half t-shirt",
+    "Gents winter yellow jacket",
+    "Men's watches brown leather",
+];
