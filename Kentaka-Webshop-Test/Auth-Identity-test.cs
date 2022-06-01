fbc47bc7978874e7d81c9619e614e6deda6685a9
saifyousifsson <saif.yousifsson@gmail.com>
2022-06-01 11:52:35 +0200

    Added auth idrntity test to my project

---
+++ Kentaka-Webshop-Test/Auth-Identity-test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kentaka_Webshop_Test
+{
+    internal class Auth_Identity_test
+    {
+    }
+}
