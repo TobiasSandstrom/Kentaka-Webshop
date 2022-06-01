60e0adb1363dc9a72bc0fae95c11f25c913d5d5d
saifyousifsson <saif.yousifsson@gmail.com>
2022-06-01 22:57:23 +0200

    Added moq package

--- Kentaka-Webshop-Test/Auth-Identity-test.cs
+++ Kentaka-Webshop-Test/Auth-Identity-test.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Kentaka_Webshop.Areas.Identity.Pages.Account;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,19 @@ namespace Kentaka_Webshop_Test
 {
     [TestFixture]
    public class Auth_Identity_test
-    {
+    { 
+        [Test]
+        public void Login_WithCorrectPaswordForexsitingUsername_ReternAccountForCorrectUsername()
+        {
+            //Arrange
+            LoginModel loginModel = new LoginModel();
+            //Act
+
+
+            //Assert
+
+
+
+        }
     }
 }
