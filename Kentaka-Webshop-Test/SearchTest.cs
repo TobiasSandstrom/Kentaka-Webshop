61eed4cdad134f04516a513cb7898842a9cdcd4b
Fleeh <turboralle@hotmail.com>
2022-06-02 12:52:57 +0200

    Added a searchtest using Xunit in our test project.

---
+++ Kentaka-Webshop-Test/SearchTest.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace Kentaka_Webshop_Test
+{
+
+         public class SearchTest
+    {
+        [Fact]
+        public void SearchTest1()
+        {
+            //ARANGE
+
+            //ACT
+
+            //ASSERT
+        }
+    }
+}
