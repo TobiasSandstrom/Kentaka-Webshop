9190ddf4ec23a9c6b08afee0a2f3c0b927670fd4
Fleeh <turboralle@hotmail.com>
2022-06-02 13:03:04 +0200

    Added code to IntegerTest for upcoming account balance in navbar

---
+++ Kentaka-Webshop-Test/IntegerTest.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace Kentaka_Webshop_Test
+{
+
+         public class IntegerTest
+    {
+        [Fact]
+        public void TestAnInteger()
+        {
+            // ARANGE
+            // Setting up test environment for future account balance.
+            int expected = 50;
+
+           
+            // ACT
+            // performing the act to be tested
+            // gathering the actual answer
+            int actual = 10 * 5;
+
+
+            // ASSERT
+            // check that expected = actual
+            Assert.Equal(expected, actual);
+        }
+    }
+}
